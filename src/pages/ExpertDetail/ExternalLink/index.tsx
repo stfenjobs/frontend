0e35441390cb78a1e6b8630e2645d069e81b2d8a
khunkin <657828489@qq.com>
2019-12-03 18:58:50 +0800

    intergrate left part

--- src/pages/ExpertDetail/ExternalLink/index.tsx
+++ src/pages/ExpertDetail/ExternalLink/index.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { BackTop, Affix, Card, Icon, Avatar } from 'antd'
 import './ExternalLink.css'
 
-const content1 = "此处应有相关推荐的图谱";
+const content_relativity = "此处应有相关推荐的图谱";
 
-const content2 = "此处应有基于时间的论文发表统计图";
+const content_post_num = "此处应有基于时间的论文发表统计图";
 
 const link = ""
-const content3 = (<a href={link}>GitHub主页</a>);
+const content_external_link = (<a href={link}>GitHub主页</a>);
 
 function ExternalLink(){
 
@@ -20,7 +20,7 @@ function ExternalLink(){
                     title="相关度"
                 >
                     <div className='content'>
-                        {content1}
+                        {content_relativity}
                     </div>
                 </Card>
                 <Card
@@ -28,7 +28,7 @@ function ExternalLink(){
                     title="发文量"
                 >
                     <div className='content'>
-                        {content2}
+                        {content_post_num}
                     </div>
                 </Card>
                 <Card
@@ -36,7 +36,7 @@ function ExternalLink(){
                     title="外链"
                 >
                     <div className='content'>
-                        {content3}
+                        {content_external_link}
                     </div>
                 </Card>
             </div>
--- src/pages/ExpertDetail/PersonalAttribute/index.tsx
+++ src/pages/ExpertDetail/PersonalAttribute/index.tsx
@@ -23,21 +23,25 @@ const avatar = 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png
 function PersonalAttribute(){
     return (
         <div className="personal-attribute">
-            <Avatar size={100} src={avatar}/>
-            <Descriptions 
-                column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
-                >
-                <Descriptions.Item label="论文数">{data[0]}</Descriptions.Item>
-                <Descriptions.Item label="被引数">{data[1]}</Descriptions.Item>
-                <Descriptions.Item label="所在组织">{organization}</Descriptions.Item>
-                <Descriptions.Item label="领域">{field}</Descriptions.Item>
-                <Descriptions.Item label="职位">{position}</Descriptions.Item>
-                <Descriptions.Item label="是否认证">{isCertificated ? '是' : '否'}</Descriptions.Item>
-                <Descriptions.Item label="个人简介">{profile} </Descriptions.Item>
-                <Descriptions.Item label="个人主页"><a href={homepage}>{homepageName}</a></Descriptions.Item>
-                <Descriptions.Item label="联系电话">{phoneNum}</Descriptions.Item>
-                <Descriptions.Item label="邮箱">{mail}</Descriptions.Item>
-            </Descriptions>
+            <div className='personal-attribute-avatar'>
+                <Avatar size={100} src={avatar} />
+            </div>
+            <div className="personal-attribute-description">
+                <Descriptions 
+                    column={{ xxl: 3, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
+                    >
+                    <Descriptions.Item label="论文数">{data[0]}</Descriptions.Item>
+                    <Descriptions.Item label="被引数">{data[1]}</Descriptions.Item>
+                    <Descriptions.Item label="所在组织">{organization}</Descriptions.Item>
+                    <Descriptions.Item label="领域">{field}</Descriptions.Item>
+                    <Descriptions.Item label="职位">{position}</Descriptions.Item>
+                    <Descriptions.Item label="是否认证">{isCertificated ? '是' : '否'}</Descriptions.Item>
+                    <Descriptions.Item label="个人简介">{profile} </Descriptions.Item>
+                    <Descriptions.Item label="个人主页"><a href={homepage}>{homepageName}</a></Descriptions.Item>
+                    <Descriptions.Item label="联系电话">{phoneNum}</Descriptions.Item>
+                    <Descriptions.Item label="邮箱">{mail}</Descriptions.Item>
+                </Descriptions>
+            </div>
         </div>
     );
 }
