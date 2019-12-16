253dafcabaa257b92a06237a2e006abca602259c
inskil <pj0713@126.com>
2019-12-16 08:12:37 +0800

    now you can upload you img to chang you head

--- src/pages/Explore/Recommend/index.tsx
+++ src/pages/Explore/Recommend/index.tsx
@@ -6,35 +6,42 @@ import Col from "antd/es/grid/col";
 
 const {Meta} = Card;
 
+const range = (end = 0, step = 1) => {
+    let arr = [];
+    for (let i = 0; i < end; i += step)
+        arr[i] = {
+            img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575071034186&di=1e10a7b740e5e3788e8ec85a441d1165&imgtype=0&src=http%3A%2F%2Fwww.jianada-qianzheng.com%2Fupfile%2F201708%2F2017081627231977.jpg",
+            name: ("Europe Street beat :" + i),
+            ds: "www.instagram.com"
+        };
+    return arr
+};
+
+const stars = range(4);
 
 export default () => {
-    const card = (
-        <Col span={6}>
+    const card = stars.map(key => (
+        <Col span={6} key={key.name}>
             <Card
                 hoverable
                 style={{width: '100%'}}
                 cover={<img alt="example"
-                            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1575071034186&di=1e10a7b740e5e3788e8ec85a441d1165&imgtype=0&src=http%3A%2F%2Fwww.jianada-qianzheng.com%2Fupfile%2F201708%2F2017081627231977.jpg"/>}
+                            src={key.img}/>}
             >
-                <Meta title="Europe Street beat" description="www.instagram.com"/>
+                <Meta title={key.name} description={key.ds}/>
             </Card>
         </Col>
-    );
+    ));
 
-    const range = (end = 0, step = 1) => {
-        let arr = []
-        for (let i = 0; i < end; i += step)
-            arr[i] = card
-        return arr
-    }
+
 
     return (
         <div style={{margin: "0", verticalAlign: "middle", padding: 0}}>
             <Row gutter={[24, 24]}>
-                {range(4)}
+                {card}
             </Row>
             <Row gutter={[24, 24]}>
-                {range(4)}
+                {card}
             </Row>
         </div>
     );
